0c736f3bbe9eaea4026a277b14223cefe82d4bd5
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-03-06 13:02:54 -0300

    feat: add user registration DTOs

---
+++ src/main/java/com/taxcalc/dto/UserResponseDTO.java
@@ -0,0 +1,31 @@
+package com.taxcalc.dto;
+
+public class UserResponseDTO {
+    private Long id;
+    private String username;
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
