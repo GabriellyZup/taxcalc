66cbec33d45760cab7385828a3867ae93731f74c
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-03-06 12:49:53 -0300

    test: add integration test for TaxType creation

---
+++ src/test/java/com/taxcalc/repository/TaxTypeRepositoryIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.taxcalc.repository;
+
+import com.taxcalc.model.TaxType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class TaxTypeRepositoryIntegrationTest {
+
+    @Autowired
+    private TaxTypeRepository taxTypeRepository;
+
+    @Test
+    void testCreateTaxType() {
+        TaxType taxType = new TaxType();
+        taxType.setName("ICMS");
+        taxType.setTaxRate(18.0);
+        taxType.setDescription("Imposto sobre mercadorias");
+
+        TaxType savedTaxType = taxTypeRepository.save(taxType);
+        assertNotNull(savedTaxType.getId());
+    }
+}
