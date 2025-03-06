790f059a8028e2cdd495aabd6d7799c50f8a754c
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-03-06 12:24:39 -0300

    chore: initial project setup with spring boot, postgresql, and basic dependencies

---
+++ src/main/java/com/taxcalc/TaxcalcApplication.java
@@ -0,0 +1,13 @@
+package com.taxcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxcalcApplication.class, args);
+	}
+
+}
