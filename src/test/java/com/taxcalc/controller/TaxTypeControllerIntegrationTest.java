7f2713f9fa93b99252bb1a8c35b865c61fb4bb79
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-03-10 10:52:24 -0300

    feat: replace doble with bigdata in tax calculation
    - Replace doble with bigdata in all modules

--- src/test/java/com/taxcalc/controller/TaxTypeControllerIntegrationTest.java
+++ src/test/java/com/taxcalc/controller/TaxTypeControllerIntegrationTest.java
@@ -1,7 +1,7 @@
 // TaxTypeControllerIntegrationTest.java (novo)
 package com.taxcalc.controller;
 
-import com.taxcalc.dto.request.TaxTypeRequest;
+import com.taxcalc.dto.TaxTypeRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
