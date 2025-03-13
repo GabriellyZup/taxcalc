49038b7a00192aab5d399485d80775920137021e
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-03-13 11:14:47 -0300

    fix: remove security configurations to enable Swagger

--- src/main/java/com/taxcalc/service/UserService.java
+++ src/main/java/com/taxcalc/service/UserService.java
@@ -8,18 +8,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 @Service
 @Tag(name = "User Service", description = "Service for user registration and management")
 public class UserService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Operation(
@@ -38,7 +36,9 @@ public class UserService {
 
         User user = new User();
         user.setUsername(dto.getUsername());
-        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+
+        user.setPassword(BCrypt.hashpw(dto.getPassword(), BCrypt.gensalt()));
+
         user.setRole(dto.getRole());
 
         User savedUser = userRepository.save(user);
